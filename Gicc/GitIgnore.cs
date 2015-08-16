0aeb88415a8cc99adce3831b88d019fcc7d8fdc9
felixdies <felixdies@daum.net>
2015-08-17 00:21:36 +0900

    implementing GitIgnore.GlobPatternToRegex - remove Glob. It doesn't work.

---
+++ Gicc/GitIgnore.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Gicc
+{
+  internal class GitIgnore
+  {
+    List<Regex> ignoreDirList;
+    List<Regex> unIgnoreDirList;
+    List<Regex> ignoreFileList;
+    List<Regex> unIgnoreFileList;
+
+    internal GitIgnore(string[] ignoredPatternArr)
+    {
+      ignoreFileList = new List<Regex>();
+      unIgnoreFileList = new List<Regex>();
+      ignoreDirList = new List<Regex>();
+      unIgnoreDirList = new List<Regex>();
+
+      ParseIgnoredPatternArr(ignoredPatternArr);
+    }
+
+    /// <summary>
+    /// 주어진 디렉토리 경로가 .gitignore 파일에 설정 돼 있는 지 여부를 반환합니다.
+    /// </summary>
+    /// <param name="dirPath"></param>
+    /// <returns></returns>
+    public bool IsIgnoredDir(string dirPath)
+    {
+      if (!dirPath.StartsWith("\\"))
+      {
+        dirPath = "\\" + dirPath;
+      }
+
+      foreach (Regex unIgnoreDir in unIgnoreDirList)
+      {
+        Match m = unIgnoreDir.Match(dirPath);
+        if (m.Success && m.Length == dirPath.Length)
+        {
+          return false;
+        }
+      }
+
+      foreach (Regex ignoreDir in ignoreDirList)
+      {
+        Match m = ignoreDir.Match(dirPath);
+        if (m.Success && m.Length == dirPath.Length)
+        {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    /// <summary>
+    /// 주어진 파일 경로가 .gitignore 파일에 설정 돼 있는 지 여부를 반환합니다.
+    /// </summary>
+    /// <param name="filePath"></param>
+    /// <returns></returns>
+    public bool IsIgnoredFile(string filePath)
+    {
+      if (!filePath.StartsWith("\\"))
+      {
+        filePath = "\\" + filePath;
+      }
+
+      foreach (Regex unIgnoreFile in unIgnoreFileList)
+      {
+        Match m = unIgnoreFile.Match(filePath);
+        if (m.Success && m.Length == filePath.Length)
+        {
+          return false;
+        }
+      }
+
+      foreach (Regex ignoreFile in ignoreFileList)
+      {
+        Match m = ignoreFile.Match(filePath);
+        if (m.Success && m.Length == filePath.Length)
+        {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    private void ParseIgnoredPatternArr(string[] ignoredPatternArr)
+    {
+      foreach (string patt in ignoredPatternArr)
+      {
+        if (patt.StartsWith("#"))
+        {
+          continue;
+        }
+        else if (patt.StartsWith("!"))
+        {
+          if (patt.EndsWith("/"))
+          {
+            this.unIgnoreDirList.Add(GlobPatternToRegex(patt.Substring(1)));
+          }
+          else
+          {
+            this.unIgnoreFileList.Add(GlobPatternToRegex(patt.Substring(1)));
+          }
+        }
+        else
+        {
+          if (patt.EndsWith("/"))
+          {
+            this.ignoreDirList.Add(GlobPatternToRegex(patt));
+          }
+          else
+          {
+            this.ignoreFileList.Add(GlobPatternToRegex(patt));
+          }
+        }
+      }
+    }
+
+    private Regex GlobPatternToRegex(string globPatt)
+    {
+      // 1. escape
+      string patt = Regex.Escape(globPatt);
+
+      // 2. parse the leading slash
+      // 
+      if (patt.StartsWith("/"))
+      {
+        patt = patt.Substring(1);
+      }
+      else
+      {
+        patt = ".*" + patt;
+      }
+
+      // 3. replace '/'
+      patt.Replace("/", "[\\/]");
+
+      // 4. parse asterisks
+
+
+      return null;
+    }
+  }
+}
---
+++ GiccTest/GitIgnoreTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using Gicc.Lib;
+
+namespace Gicc.Test
+{
+  [TestFixture]
+  public class GitIgnoreTest
+  {
+    [TestCase(@".builds", true)]
+    [TestCase(@"0.suo", true)]
+    [TestCase(@"sub\0.suo", true)]
+    [TestCase(@"sub/0.suo", true)]
+    [TestCase(@"0.sln.docstates", true)]
+    [TestCase(@"0.ide\0", true)]
+    [TestCase(@"0.sln.ide\0", true)]
+    [TestCase(@"debug\0", true)]
+    [TestCase(@"Debug\0", true)]
+    [TestCase(@"x86\0", true)]
+    [TestCase(@"testresult\0", true)]
+    [TestCase(@"testresult1/0", true)]
+    [TestCase(@"testResult\0", true)]
+    [TestCase(@"Testresult1/0", true)]
+		[TestCase(@"file", false)]
+    [TestCase(@"dir\0", false)]
+    public void FnMatch(string path, bool expectedResult)
+    {
+      string[] ignoreArr = Resource.GetResource(Resource.GetResource("gitignore.txt")).Split('\n');
+
+      GitIgnore gitIgnore = new GitIgnore(ignoreArr);
+
+      Assert.AreEqual(expectedResult, gitIgnore.IsIgnoredFile(path));
+    }
+  }
+}
